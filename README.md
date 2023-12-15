2d93a40fea5e095a9306069146d91474ff828b41
Livia Coutinho <livia.coutinho@sou.inteli.edu.br>
2023-12-15 15:22:51 -0300

    prova

--- README.md
+++ README.md
@@ -1,2 +1,68 @@
 # 20232BSET03P2
 Inteli - Engenharia de Software | Avaliação 2023-2B P2
+
+#### Aluna: Livia L Coutinho
+
+## Letra b) --> Sanitizar e validar dados de entrada para evitar SQL Injection.
+
+- Mudanças no esquema das tabelas:
+
+Adição de "INTEGER PRIMARY KEY" nas colunas "id" das tabelas "cats" e "dogs" - chave primária única para cada linha na tabela e permite a identificação exclusiva de cada registro.
+
+- Prepared Statements:
+
+As queries SQL foram alteradas para uso de prepared statements -  os valores dos parâmetros são passados separadamente das instruções SQL, evitando a interpolação direta de strings e ajudando a prevenir SQL Injection.
+
+- Sanitizar dados de entrada:
+
+Uso de placeholders (?) nos comandos SQL e passar os valores fornecidos separadamente. 
+
+- Finalizar as declarações:
+
+Após a execução das queries preparadas, há a inclusão de stmt.finalize();. Isso garante que as declarações preparadas sejam encerradas após a execução, liberando recursos.
+
+- GET:
+
+Adição de rotas GET para cats and dogs para buscar dados das tabelas correspondentes. 
+
+## Letra c) --> Corrigir a lógica de votação para que verifique se o registro do animal existe antes de adicionar um voto.
+
+- animalExists
+
+Adição da função animalExists que recebe o nome da tabela (animalType), o ID do animal e um callback para retornar se o animal existe ou não no banco de dados.
+
+- (/vote/:animalType/:id)
+
+Nas rotas de votação , foi chamada a função animalExists para verificar se o animal existe antes de incrementar os votos.
+
+- Verificação
+
+Verificações para retornar mensagens apropriadas se o animal não for encontrado ou se ocorrerem erros ao verificar a existência ou ao atualizar os votos.
+
+## Letra d) --> Implementar e tratar erros de maneira adequada, sem vazar detalhes de implementação.
+
+- Tratamento de erro na função animalExists, enviando uma mensagem apropriada para o erro.
+- A função animalExists agora chama o callback sem enviar o erro em caso de sucesso.
+- Adição de finalização do statement stmt após o seu uso para prevenir vazamento de recursos.
+- Correção do nome da variável err para error na função de callback.
+- Envio do erro recebido na verificação de existência do animal na rota de votação.
+- Finalização do statement stmt após o seu uso na rota de votação para evitar vazamento de recursos (letra a)
+
+## letra e) --> Implementar todos os métodos que possuem assinatura no código.
+
+Alterações feitas:
+
+Tratamento de erros:
+- --> verificar a existência do animal.
+- --> ajuste na passagem de erros nas funções callback.
+
+Finalização adequada:
+- --> comandos para finalizar operações no banco de dados após o uso.
+
+Correção de erros na rota de votação:
+- --> tratamento apropriado de erros na rota de votação.
+
+</br> </br>
+![Alt text](image-1.png)
+</br> </br>
+![Alt text](image.png)
--- index.js
+++ index.js
@@ -1,23 +1,24 @@
-const express = require('express');
-const sqlite3 = require('sqlite3').verbose();
-const bodyParser = require('body-parser');
+function animalExists(tableName, id, callback) {           // verifica se o animal existe
+  const query = `SELECT COUNT(*) as count FROM ${tableName} WHERE id = ?`;
+  db.get(query, [id], (err, row) => {
+    if (err) {
+      // Tratamento do erro com retorno adequado
+      callback(false, "Erro ao verificar a existência do animal");
+    } else {
+      const exists = row.count > 0;
+      callback(exists, null); //Callback sem enviar erro em caso de sucesso
+    }
+  });
+}
 
-const app = express();
-const port = 3000;
+app.post('/dogs', (req, res) => {      // inserir dados na tabela dogs usando prepared statement
 
-app.use(bodyParser.json());
-
-
-const db = new sqlite3.Database(':memory:');
-
-db.serialize(() => {
-  db.run("CREATE TABLE cats (id INT, name TEXT, votes INT)");
-  db.run("CREATE TABLE dogs (id INT, name TEXT, votes INT)");
-});
-
-app.post('/cats', (req, res) => {
   const name = req.body.name;
-  db.run(`INSERT INTO cats (name, votes) VALUES ('${name}', 0)`, function(err) {
+
+  const stmt = db.prepare("INSERT INTO dogs (name, votes) VALUES (?, ?)");
+  stmt.run(name, 0, function(err) {
+    stmt.finalize(); // finalização do statement após uso
+
     if (err) {
       res.status(500).send("Erro ao inserir no banco de dados");
     } else {
@@ -26,35 +27,78 @@ app.post('/cats', (req, res) => {
   });
 });
 
-app.post('/dogs', (req, res) => {
-  
-});
-
+// Rota de votação
 app.post('/vote/:animalType/:id', (req, res) => {
- 
-  db.run(`UPDATE ${animalType} SET votes = votes + 1 WHERE id = ${id}`);
-  res.status(200).send("Voto computado");
-});
+  const animalType = req.params.animalType;
+  const id = req.params.id;
 
-app.get('/cats', (req, res) => {
-  db.all("SELECT * FROM cats", [], (err, rows) => {
-    if (err) {
-      res.status(500).send("Erro ao consultar o banco de dados");
+  animalExists(animalType, id, (exists, error) => { // correção do nome da variável para error
+
+    if (error) {
+      res.status(500).send(error); // envio do erro recebido na verificação
+    } else if (!exists) {
+      res.status(404).send("Animal não encontrado");
     } else {
-      res.json(rows);
+      const stmt = db.prepare(`UPDATE ${animalType} SET votes = votes + 1 WHERE id = ?`);
+      stmt.run(id, function(err) {
+        stmt.finalize(); // finalização do statement após uso
+
+        if (err) {
+          res.status(500).send("Erro ao atualizar os votos no banco de dados");
+        } else {
+          res.status(200).send("Voto computado");
+        }
+      });
     }
   });
 });
 
-app.get('/dogs', (req, res) => {
-  
+// Rota para obter todos os gatos
+app.get('/cats', (req, res) => {
+  animalExists('cats', req.params.id, (exists, error) => {
+    if (error) {
+      res.status(500).send(error);
+    } else if (!exists) {
+      res.status(404).send("Gato não encontrado");
+    } else {
+      db.all("SELECT * FROM cats", [], (err, rows) => {
+        if (err) {
+          res.status(500).send("Erro ao consultar o banco de dados de gatos");
+        } else {
+          res.json(rows);
+        }
+      });
+    }
+  });
 });
 
+// Rota para obter todos os cachorros
+app.get('/dogs', (req, res) => {
+  animalExists('dogs', req.params.id, (exists, error) => {
+    if (error) {
+      res.status(500).send(error);
+    } else if (!exists) {
+      res.status(404).send("Cachorro não encontrado");
+    } else {
+      db.all("SELECT * FROM dogs", [], (err, rows) => {
+        if (err) {
+          res.status(500).send("Erro ao consultar o banco de dados de cachorros");
+        } else {
+          res.json(rows);
+        }
+      });
+    }
+  });
+});
+
+
+
+// tratamento de erros
 app.use((err, req, res, next) => {
   console.error(err.stack);
-  res.status(500).send('Ocorreu um erro!');
+  res.status(500).send('Ocorreu um erro');
 });
 
 app.listen(port, () => {
-  console.log(`Cats and Dogs Vote app listening at http://localhost:${port}`);
+  console.log(`Votação cats and dogs rodando em http://localhost:${port}`);
 });
